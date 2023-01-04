06ccb1d9785b0f53b1027b93811313718082d2a2
ashutoshkumarmangal <aashuakm12@gmail.com>
2023-01-04 15:30:07 +0530

    cp

---
+++ A_Ultra_Fast_Mathematician.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class A_Ultra_Fast_Mathematician{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a  = sc.next();
+    String b = sc.next();
+    char ch[] = new char[a.length()];
+    for(int i =0;i<a.length();i++){
+        
+    }
+
+}
+}
